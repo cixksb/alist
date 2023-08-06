c7b797c29549ae0726c7249d08cd46d81a0c4b49
Leroy Kuen <141104408+cixksb@users.noreply.github.com>
2023-08-06 10:46:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ builtAt="$(date +'%F %T %z')"
 goVersion=$(go version | sed 's/go version //')
 gitAuthor="Xhofe <i@nn.ci>"
 gitCommit=$(git log --pretty=format:"%h" -1)
-
-
+##
 if [ "$1" = "dev" ]; then
   version="dev"
   webVersion="dev"
@@ -12,12 +11,10 @@ else
   version=$(git describe --abbrev=0 --tags)
   webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/alist-org/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
-
-
+##
 echo "backend version: $version"
 echo "frontend version: $webVersion"
-
-
+##
 ldflags="\
 -w -s \
 -X 'github.com/alist-org/alist/v3/internal/conf.BuiltAt=$builtAt' \
